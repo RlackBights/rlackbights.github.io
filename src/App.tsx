100ef2e5a053b9ece9b9172c72042cef0dd9b5f4
RlackBights <hunor.n.b@gmail.com>
2024-04-18 12:03:17 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h1>My personal page, cool</h1>
+    </div>
+  );
+}
+
+export default App;
