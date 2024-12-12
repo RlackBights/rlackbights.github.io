3f299e407eecd51f4271633eb1a34fabc37837b2
RlackBights <114919830+RlackBights@users.noreply.github.com>
2024-12-12 21:17:45 +0100

    Added DM App link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,6 +11,7 @@ function Navbar() {
                     <a href="https://github.com/RlackBights">GitHub</a>
                     <a href="https://rlackbights.github.io/anki-card-generator">Anki Card Generator</a>
                     <a href="https://rlackbights.github.io/uni-calendar-manager">Uni Calendar Manager</a>
+                    <a href="https://rlackbights.github.io/dm-app">DM App</a>
                 </div>
             </li>
         </ul>
